2ea1788ea48773a9a8ff5c6e8722c8d8423c8db2
HannahChristmas <hc109909@gmail.com>
2023-08-24 14:04:50 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './components/About'
+import Projects from './components/Projects'
 
 function App() {
   return (
     <div className="App">
+      <About></About>
+      <Projects></Projects>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import Headshot from '../photos/Headshot.png'
+
+function About() {
+    return (
+        <div>
+            <h1>Hannah Christmann</h1>
+            <img id="headshot" alt="headshot" src={Headshot}></img>
+            <h3>full-stack software engineer</h3>
+            <p>Software Engineer with a focus on testing and quality- channeling my critical thinking, problem solving, and management skills into each of my projects. Full-stack experience building web applications with JavaScript, React.js, Ruby on Rails, PostgreSQL, CSS, and HTML. Experience with testing automation using Cypress e2e, manual testing, and quality assurance practices.<br></br><br></br>
+            I am looking to step into a role that will allow me to continue developing my skills and expand my technical experience. Open to relocation and remote work.<br></br><br></br>
+            Currently learning: mobile development and Java backend.</p>
+        </div>
+    )
+}
+
+export default About;
